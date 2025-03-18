2565ec0deb801b0ee64d7eed182285df473aba98
Paul CLIQUE <paulclique@MacBook-Pro-de-Paul.local>
2025-03-18 16:39:25 +0100

    Fix missing app keys in config/server.js

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
+    keys: env.array('APP_KEYS', ['U3ANSNScnkCkVaB7y8562g89kUq87s', 'gPSjkCJe7n8Zc6GtFL44m37z666NvE'])
+  }
 });
